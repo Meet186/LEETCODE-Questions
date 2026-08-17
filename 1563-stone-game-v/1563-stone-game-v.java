8d1b2e62cdc1422dc19d43233fb3b67da25c048b
Meet Solanki <149171435+Meet186@users.noreply.github.com>
2026-08-17 12:36:44 +0530

    Time: 363 ms (13.7%), Space: 47.7 MB (49.28%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.java
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -1,47 +1,46 @@
 class Solution {
     public int stoneGameV(int[] stoneValue) {
-        int totalSum = 0;
         int n = stoneValue.length;
 
         int[][] dp = new int[n][n];
-        for(int i=0;i<n;i++) Arrays.fill(dp[i], -1);
 
-        return helper(0, n-1, stoneValue, dp);
-    }
+        for(int i=0;i<n;i++) dp[i][i] = 0;
 
-    private int helper(int left, int right, int[] stoneValue, int[][] dp){
-        if(left == right) return 0;
+        for(int left=n-1;left>=0;left--){
+            for(int right=0;right<n;right++){
+                int totalSum = 0;
 
-        if(dp[left][right] != -1) return dp[left][right];
+                for(int i=left;i<=right;i++){
+                    totalSum += stoneValue[i];
+                }
 
-        int totalSum = 0;
+                int leftSum = 0;
+                int rightSum = 0;
 
-        for(int i=left;i<=right;i++){
-            totalSum += stoneValue[i];
+                int maxScore = 0;
+
+                for(int i=left;i<=right-1;i++){
+                    leftSum += stoneValue[i];
+                    rightSum = totalSum-leftSum;
+                    
+                    if(leftSum == rightSum){
+                        int l = leftSum + dp[i+1][right];
+                        int r = rightSum + dp[left][i];
+                        maxScore = Math.max(maxScore, Math.max(l, r));
+                    }
+                    else if(leftSum < rightSum){
+                        maxScore = Math.max(maxScore ,leftSum+dp[left][i]);
+                    }
+                    else{
+                        maxScore = Math.max(maxScore, rightSum + dp[i+1][right]);
+                    }
+                }
+                
+                dp[left][right] = maxScore;
+            }
         }
 
-        int leftSum = 0;
-        int rightSum = 0;
-
-        int maxScore = 0;
-
-        for(int i=left;i<=right-1;i++){
-            leftSum += stoneValue[i];
-            rightSum = totalSum-leftSum;
-            
-            if(leftSum == rightSum){
-                int l = leftSum + helper(i+1, right, stoneValue, dp);
-                int r = rightSum + helper(left, i, stoneValue, dp);
-                maxScore = Math.max(maxScore, Math.max(l, r));
-            }
-            else if(leftSum < rightSum){
-                maxScore = Math.max(maxScore ,leftSum+helper(left, i, stoneValue, dp));
-            }
-            else{
-                maxScore = Math.max(maxScore, rightSum + helper(i+1, right, stoneValue, dp));
-            }
-        }
-        
-        return dp[left][right] = maxScore;
+        return dp[0][n-1];
     }
+
 }
